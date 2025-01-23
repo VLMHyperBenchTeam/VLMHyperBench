c6b55e19ba8cb684cd6998b9054b1383be7ce0ab
Semyon W232 <lukinsemyon@yandex.ru>
2025-01-23 06:06:04 +0500

    almost heaven

--- run_benchmark.py
+++ run_benchmark.py
@@ -1,6 +1,6 @@
 import os
 
-from benchmark_scheduler.benchmark_scheduler import run_container
+from benchmark_scheduler.docker_run import run_container
 
 
 if __name__ == "__main__":
@@ -21,11 +21,11 @@ if __name__ == "__main__":
         container_dir=container_directory,
         script_path="bench_stages/run_vlm.py",
         packages_to_install=[
-            "wheels/some_package-0.1.0-py3-none-any.whl",
             "git+https://github.com/VLMHyperBenchTeam/model_interface.git@0.1.0",
+            "git+https://github.com/VLMHyperBenchTeam/benchmark_run_config.git@0.1.0",
             "git+https://github.com/VLMHyperBenchTeam/model_qwen2-vl.git@0.1.0",
-            # "wheels/model_interface-0.1.0-py3-none-any.whl",
-            # "wheels/model_qwen2_vl-0.1.0-py3-none-any.whl",
+            "git+https://github.com/VLMHyperBenchTeam/dataset_iterator.git@0.1.0",
+            # "git+https://github.com/VLMHyperBenchTeam/system_prompt_adapter.git@0.1.0",
         ],
         keep_container=False,
         use_gpu=True,
--- vlmhyperbench/bench_stages/run_vlm.py
+++ vlmhyperbench/bench_stages/run_vlm.py
@@ -1,39 +1,10 @@
 import os
-import time
 
-import torch
 from model_interface.model_factory import ModelFactory
-from some_package.my_module import Calculator, greet
-from tqdm import tqdm
+from benchmark_run_config.benchmark_run_config import BenchmarkRunConfig
+from dataset_iterator.fabrics import IteratorFabric
 
 if __name__ == "__main__":
-    # Имя файла, в который будем записывать данные
-    filename = "data/task1_output.txt"
-
-    # Тест модуля добавленного в докер-контейнер
-    print("Тестируем some_package")
-    print("-" * 30)
-    print(greet(name="Ivan"))
-    calc = Calculator()
-    print(calc.add(1, 3))
-    print("-" * 30)
-
-    os.system("nvidia-smi")
-    print("torch.__version__", torch.__version__)
-    print("torch.cuda.is_available()", torch.cuda.is_available())
-    print("torch.cuda.current_device()", torch.cuda.current_device())
-    print("torch.cuda.device_count()", torch.cuda.current_device())
-    print("torch.cuda.get_device_name(0)", torch.cuda.get_device_name(0))
-
-    with open(filename, "w") as file:
-        for i in tqdm(range(5), desc="Processing", unit="iteration"):
-            line = i
-            file.write(f"{i}\n")
-            print(f"\nResult: {line}")
-            file.flush()
-
-            # Ждем одну секунду
-            time.sleep(1)
 
     # Пример работы с моделями семейства Qwen2-VL
     cache_directory = "model_cache"
@@ -42,35 +13,44 @@ if __name__ == "__main__":
     script_dir = os.path.dirname(os.path.abspath(__file__))
     cache_directory = os.path.join(script_dir, cache_directory)
 
-    # Имена моделей и семейство моделей
-    model_name_1 = "Qwen2-VL-2B-Instruct"
-    model_family = "Qwen2-VL"
+    # Получаем конфиг со всеми параметрами прогона
+    config_dir = os.path.join(script_dir, "BenchmarkRunConfig.json")
+    config = BenchmarkRunConfig.from_json(config_dir)
 
     # Инфо о том где взять класс для семейства моделей
-    package = "model_qwen2_vl"
-    module = "models"
-    model_class = "Qwen2VLModel"
-    model_class_path = f"{package}.{module}:{model_class}"
+    model_class_path = f"{config.python_package}.{config.module}:{config.class_name}"
 
     # Регистрация модели в фабрике
+    model_family = "Qwen2-VL"
     ModelFactory.register_model(model_family, model_class_path)
 
-    image_path = "example_docs/schet_na_oplatu.png"
-    question = "Пожалуйста собери следующую информацию с документа:покупатель,\r\nИНН покупателя,\r\nКПП покупателя,\r\nтелефон покупателя,\r\nпоставщик,\r\nИНН поставщика,\r\nБИК поставщика,\r\nКор. счет поставщика,\r\nР/с поставщика,\r\nдата документа,\r\nномер счета документа,\r\nПеречисли каждый купленный товар  (наименование, количество, цена за штуку)\r\nкакую сумму нужно заплатить за все товары\r\nв какой валюте платим,\r\nвзнос НДС.\r\nВерни ответ в виде json файла с полями и ответами на них."
+    # TODO: Получаем системный промпт
+    # system_prompt_adapter = ...
 
-    # создаем модель
+    # параметры модели - передаем системный промпт, если доступен
     model_init_params = {
-        "model_name": model_name_1,
+        "model_name": config.model_name,
         "system_prompt": "",
         "cache_dir": "model_cache",
     }
 
     model = ModelFactory.get_model(model_family, model_init_params)
 
+    # Совершаем прогон по датасету
+    task_name = "VQA"
+    iterator = IteratorFabric.get_dataset_iterator(task_name=task_name, 
+                                                   dataset_name=config.dataset,
+                                                   filter_doc_class=config.filter_doc_class,
+                                                   filter_question_type=config.filter_question_type)
+    runner = IteratorFabric.get_runner(iterator, model)
+    runner.run()
     # отвечаем на вопрос о по одной картинке
-    model_answer = model.predict_on_image(image=image_path, question=question)
+    # image_path = "example_docs/schet_na_oplatu.png"
+    # question = "Пожалуйста собери следующую информацию с документа:покупатель,\r\nИНН покупателя,\r\nКПП покупателя,\r\nтелефон покупателя,\r\nпоставщик,\r\nИНН поставщика,\r\nБИК поставщика,\r\nКор. счет поставщика,\r\nР/с поставщика,\r\nдата документа,\r\nномер счета документа,\r\nПеречисли каждый купленный товар  (наименование, количество, цена за штуку)\r\nкакую сумму нужно заплатить за все товары\r\nв какой валюте платим,\r\nвзнос НДС.\r\nВерни ответ в виде json файла с полями и ответами на них."
+
+    # model_answer = model.predict_on_image(image=image_path, question=question)
 
     # отвечаем на вопрос о по нескольким картинкам сразу (пока не реализован)
     # model_answer = model.predict_on_images(images=[image_path1, image_path2], question=question)
 
-    print(model_answer)
+    # print(model_answer)
