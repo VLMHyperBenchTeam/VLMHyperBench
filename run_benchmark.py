ceafb4fd664cf72e0db7030c297ce356f39c2ea8
Anton Shiryaev <med.phisiker@gmail.com>
2025-03-21 20:30:48 +1000

    feat(VLMHyperBench cfg): add user_config, benchmark_run_cfg, vlm_base
    add user_config, benchmark_run_cfg, vlm_base

--- run_benchmark.py
+++ run_benchmark.py
@@ -9,27 +9,28 @@ from config_manager.config_manager import ConfigManager
 if __name__ == "__main__":
     # Загрузка конфига из файла
     cfg_path = "vlmhyperbench/cfg/VLMHyperBench_config.json"
-    run_cfg = BenchmarkRunConfig.from_json("vlmhyperbench/cfg/BenchmarkRunConfig.json")
 
     # Считываем конфиг для VLMHyperBench
-    config = ConfigManager(cfg_path)
+    evalkit_config = ConfigManager(cfg_path)
 
     # Получим маппинг директорий для Docker-контейнера
-    volumes = config.get_volumes()
+    volumes = evalkit_config.get_volumes()
     volumes = host_paths_to_abs(volumes)
 
     # Получим список python-пакетов для каждого этапа работы
-    vlm_run_packages = config.load_packages("vlm_run")
-    eval_run_packages = config.load_packages("eval_run")
+    vlm_run_packages = evalkit_config.load_packages("vlm_run")
+    eval_run_packages = evalkit_config.load_packages("eval_run")
 
     environment = load_env_vars()
+    
+    eval_run_cfg = BenchmarkRunConfig.from_json(evalkit_config.cfg["benchmark_run_cfg"])
 
     # 2. Этап "Запуск VLM" на Docker-контейнере
-    vlm_run_packages.append(run_cfg.git_python_package)
+    vlm_run_packages.append(eval_run_cfg.git_python_package)
     print(vlm_run_packages)
 
     run_container(
-        run_cfg.docker_image,
+        eval_run_cfg.docker_image,
         volumes,
         script_path="/workspace/bench_stages/run_vlm.py",
         packages_to_install=vlm_run_packages,
@@ -38,7 +39,7 @@ if __name__ == "__main__":
 
     # 3. Этап "Оценка метрик" на Docker-контейнере
     run_container(
-        config.cfg["eval_docker_img"],
+        evalkit_config.cfg["eval_docker_img"],
         volumes,
         script_path="bench_stages/run_eval.py",
         packages_to_install=eval_run_packages,
